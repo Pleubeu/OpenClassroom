7e1227fb00e2f3b8a699ca705156fd3dbb7edf78
Pleubeu <paulineliat@gmail.com>
2020-10-30 14:39:54 +0100

    Modif...

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -2,6 +2,27 @@ import React from 'react'
 import {StyleSheet, View, Text} from 'react-native'
 
 class FilmDetail extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            film: undefined,
+            isLoading: true
+        }
+
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' color='#808080' />
+                </View>
+            )
+        }
+    }
+
+
+
     render () {
         const idFilm = this.props.navigation.state.params.idFilm
         return (
@@ -16,6 +37,15 @@ class FilmDetail extends React.Component {
 const styles = StyleSheet.create({
     main_container: {
         flex: 1
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
