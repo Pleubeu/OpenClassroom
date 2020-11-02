db0f4eec2477c16d3af710aa982979a3a08dc195
Pleubeu <paulineliat@gmail.com>
2020-11-02 19:13:43 +0100

    Fin chap

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {StyleSheet, View, Text, ActivityIndicator} from 'react-native'
+import {StyleSheet, View, Text, ActivityIndicator, Image} from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
-import { getFilmDetailFromApi} from '../API/TMDBApi'
+import { getFilmDetailFromApi, getImageFromApi} from '../API/TMDBApi'
 
 
 class FilmDetail extends React.Component {
@@ -30,7 +30,30 @@ class FilmDetail extends React.Component {
         if (film != undefined) {
             return (
                 <ScrollView style={styles.scrollview_container}>
-                    <Text>{film.title}</Text>
+                            <Image
+                                style={styles.image}
+                                source={{uri: getImageFromApi(film.backdrop_path)}}
+                            />
+                            <View style={styles.content_container}>
+
+                                <View style={styles.title_container}>
+                                <Text style={styles.title_text}>{film.title}</Text>
+                                </View>
+
+                                <View style={styles.description_container}>
+                                <Text style={styles.description_text}>{film.overview}</Text>
+                                </View>
+
+                                <View style={styles.details_container}>
+                                <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
+                                <Text style={styles.vote_text}>Note : {film.vote_average}/10</Text>
+                                <Text style={styles.vote_text}>Nombres de votes : {film.vote_count}</Text>
+                                <Text style={styles.vote_text}>Budget : {film.budget}</Text>
+                                {/*<Text style={styles.vote_text}>Genre(s) : {film.genres}</Text>
+                                <Text style={styles.vote_text}>Compagnie(s) : {film.production_companies}</Text>
+                                */}
+                                </View>
+                            </View>
                 </ScrollView>
             )
         }
@@ -73,9 +96,55 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
+
+
+
+
+    
     scrollview_container: {
+        flex: 1,
+        width: "100%"
+    },
+
+    image: {
+
+        height: 200,
+        margin: 5,
+        backgroundColor: 'gray'
+      },
+      content_container: {
+        flex: 1,
+        margin: 5
+      },
+      title_container: {
+        flex: 1,
+        alignItems: "center"
+      },
+      title_text: {
+        fontWeight: 'bold',
+        fontSize: 30
+      },
+      vote_text: {
+
+      },
+      description_container: {
+        flex: 1,
+        paddingBottom: 20,
+        paddingTop: 20
+        
+      },
+      description_text: {
+        fontStyle: 'italic',
+        color: '#666666',
+        fontSize: 20
+
+      },
+      date_container: {
         flex: 1
-    }
+      },
+      date_text: {
+        fontSize: 14
+      }
 })
 
 export default FilmDetail
